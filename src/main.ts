174a09c3fdbdc283e53aaa38431b877544a55245
unknown <anterebol@gmail.com>
2022-08-06 01:52:08 +0300

    refactor: resolve conflicts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = process.env.PORT
@@ -7,4 +8,5 @@ async function bootstrap() {
     console.log(`Express server listening on port ${PORT}`);
   });
 }
+
 bootstrap();
