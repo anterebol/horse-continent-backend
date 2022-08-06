73884632cd5d1fdac6fe99fa66e16fc342e93816
unknown <anterebol@gmail.com>
2022-08-06 15:50:02 +0300

    fix: resolve conflict with CORS policy

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from "cors";
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = process.env.PORT
-  app.listen(PORT || 3000, function(){
-    console.log(`Express server listening on port ${PORT}`);
+  app.use(cors())
+  app.listen(PORT || 4000, function(){
+    console.log(`Server listening on port ${PORT}`);
   });
 }
 bootstrap();
+
