39abeba3e9382f8dad17643b28bb7f1868e22ea2
unknown <anterebol@gmail.com>
2023-03-24 00:16:22 +0300

    fix: fix cors policy

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,9 @@ import * as cors from "cors";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = process.env.PORT
-  // app.use(cors({
-  //   origin: '*'
-  // }))
+  app.use(cors({
+    origin: '*'
+  }))
   app.listen(PORT || 4000, function(){
     console.log(`Server listening on port ${PORT}`);
   });
